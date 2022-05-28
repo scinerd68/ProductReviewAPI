6f3bdc7a7d45e36db80f309747cdd85d5b9b0064
Phan Thang <thangphan0168@gmail.com>
2022-05-28 16:43:42 +0700

    Add scrape sendo to API

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template
 from flask_restful import Resource, Api
+from scrape.sendo_scrape import scrape_sendo
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+CHROME_DRIVER_PATH = "D:/chromedriver.exe"
 
 app = Flask(__name__)
 api = Api(app)
@@ -8,11 +13,15 @@ api = Api(app)
 def home():
     return render_template('index.html')
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+class GetReview(Resource):
+    def get(self, url: str):
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        driver = webdriver.Chrome(CHROME_DRIVER_PATH, options=chrome_options)
+        result = scrape_sendo(driver, url, max_review_num=5, verbose=True)
+        return result
 
-api.add_resource(HelloWorld, '/hello')
+api.add_resource(GetReview, '/review')
 
 if __name__ == '__main__':
     app.run(debug=True)
