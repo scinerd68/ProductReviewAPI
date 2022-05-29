0cdf4a31da853cb6b39a97e11b04be878f3b83de
Phan Thang <thangphan0168@gmail.com>
2022-05-29 17:12:12 +0700

    Add response when scrape non supported site

--- app.py
+++ app.py
@@ -29,6 +29,10 @@ class GetReview(Resource):
     def get(self):
         url = request.args['url']
         site = request.args['site']
+
+        if site not in ['sendo', 'lazada', 'tiki']:
+            return f"Argument value '{site}' is not supported", 400
+
         chrome_options = Options()
         chrome_options.add_argument("--headless")
         driver = webdriver.Chrome(CHROME_DRIVER_PATH, options=chrome_options)
@@ -38,7 +42,7 @@ class GetReview(Resource):
         elif site == 'lazada':
             result = scrape_lazada(driver, url, 4)
         # elif site == 'tiki':
-        #     result = scrape_tiki(driver, url, json_file)
+        #     result = scrape_tiki(driver, url, json_file)        
 
         driver.quit()
         return result
