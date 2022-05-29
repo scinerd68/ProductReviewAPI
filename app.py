ae60df007acb59dbaffbe016240021c5eebf7542
Phan Thang <thangphan0168@gmail.com>
2022-05-29 09:52:06 +0700

    Switch to get request in API, return scrape result of sendo and lazada

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 from flask_restful import Resource, Api
+from scrape.LazadaScraping import scrape_lazada
 from scrape.sendo_scrape import scrape_sendo
+from scrape.tiki_scrape import scrape_tiki
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
@@ -14,16 +16,32 @@ def home():
     return render_template('index.html')
 
 class GetReview(Resource):
-    def put(self):
-        # TODO: sendo has global start variable in scrape function causing bug
-        # TODO: Switch to GET request and parse request.args
-        url = request.form['url']
+    # def put(self):
+    #     # TODO: Switch to GET request and parse request.args
+    #     url = request.form['url']
+    #     chrome_options = Options()
+    #     chrome_options.add_argument("--headless")
+    #     driver = webdriver.Chrome(CHROME_DRIVER_PATH, options=chrome_options)
+    #     result = scrape_sendo(driver=driver, url=url, max_review_num=5, verbose=True)
+    #     driver.quit()
+    #     return result
+
+    def get(self):
+        url = request.args['url']
+        site = request.args['site']
         chrome_options = Options()
         chrome_options.add_argument("--headless")
         driver = webdriver.Chrome(CHROME_DRIVER_PATH, options=chrome_options)
-        result = scrape_sendo(driver=driver, url=url, max_review_num=5, verbose=True)
+
+        if site == 'sendo':
+            result = scrape_sendo(driver=driver, url=url, max_review_num=5, verbose=True)
+        elif site == 'lazada':
+            result = scrape_lazada(driver, url, 4)
+        # elif site == 'tiki':
+        #     result = scrape_tiki(driver, url, json_file)
+
         driver.quit()
-        return url
+        return result
 
 api.add_resource(GetReview, '/review')
 
--- test_api.py
+++ test_api.py
@@ -1,5 +1,7 @@
 import requests
 
 url = "https://www.sendo.vn/op-lung-iphone-6-plus-6s-plus-chong-soc-360-17878754.html?source_block_id=feed&source_page_id=home&source_info=desktop2_60_1653314071212_17f349b3-3df6-4ea5-9ae7-ae076bbd7e9b_-1_ishyperhome0_0_11_22_-1" # 2 pages
-result = requests.put("http://localhost:5000/review", data={"url": url})
+# url = r'https://www.lazada.vn/products/dien-thoai-apple-iphone-13-pro-max-128gb-i1522497182-s6393590575.html?search=1&spm=a2o4n.searchlistcategory.list.i72.75bf3a1fbXB2jM'
+
+result = requests.get("http://localhost:5000/review", params={"url": url, "site": "sendo"})
 print(result.json())
