8ff5f74fdeb4e673657c4dd3b208c0af2382b2a2
Phan Thang <thangphan0168@gmail.com>
2022-06-29 13:14:59 +0700

    Save to json file in test_api.py

--- test_api.py
+++ test_api.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 bs_api_key = 'DITF>NGE'
 legit_api_key = 'ab837f0cf3f14b9a9fce78265dc45076'
@@ -25,3 +26,5 @@ url = "https://www.sendo.vn/op-lung-iphone-6-plus-6s-plus-chong-soc-360-17878754
 result = requests.get("http://localhost:5000/review/byname", params={"api_key":legit_api_key, "name": "tủ lạnh lg", "site": "sendo"})
 
 print(result.json())
+with open('result.json', 'w', encoding='utf-8') as f:
+    json.dump(result.json(), f, indent=4)
