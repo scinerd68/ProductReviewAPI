4ba2a501dc2dc611958fb2612aac24e6c2174e25
Phan Thang <thangphan0168@gmail.com>
2022-07-03 20:59:19 +0700

    test scrape by name on 3 sites, lazada still not working

--- test_api.py
+++ test_api.py
@@ -8,7 +8,7 @@ legit_api_key = 'ab837f0cf3f14b9a9fce78265dc45076'
 url = "https://www.sendo.vn/op-lung-iphone-6-plus-6s-plus-chong-soc-360-17878754.html?source_block_id=feed&source_page_id=home&source_info=desktop2_60_1653314071212_17f349b3-3df6-4ea5-9ae7-ae076bbd7e9b_-1_ishyperhome0_0_11_22_-1" # 2 pages
 result = requests.get("http://localhost:5000/review/byname", params={"api_key":legit_api_key, 
                                                                      "name": "tủ lạnh lg",
-                                                                     "site": "sendo"})
+                                                                     "site": "tiki"})
 
 pprint.pprint(result.json())
 with open('result.json', 'w', encoding='utf-8') as f:
