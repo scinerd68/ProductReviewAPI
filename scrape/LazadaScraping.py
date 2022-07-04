408c61ac5627af884e3ac50110997d1f8fb520ca
HoangNV2001 <hoangnguyenviet1611@gmail.com>
2022-07-04 11:18:41 +0700

    rework scraper

--- scrape/LazadaScraping.py
+++ scrape/LazadaScraping.py
@@ -12,24 +12,27 @@ def scrape_lazada(driver, url, max_comment = 5):
     driver.get(url)
     result = {'product_name':'','avg_rating':0,'source':'lazada','reviews':[]}
     review_count = 0
+    result['product_name'] = driver.find_element(By.CSS_SELECTOR, "[class='pdp-mod-product-badge-title']").text
+
 
     #click out pop up
-    driver.implicitly_wait(10)
     ac = ActionChains(driver)
     ac.move_by_offset(1, 1).click().perform()
 
-
     x = 0
-    while x < max_comment:
-        driver.implicitly_wait(10)
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight/2);")
-        driver.implicitly_wait(5)
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight*0.7);")
-        driver.implicitly_wait(10)
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight*0.8);")
+    while True:
+        y = 1000
+        for timer in range(0,8):
+             driver.execute_script("window.scrollTo(0, "+str(y*0.4)+")")
+             y += 1000
+             time.sleep(1)
 
-        product_reviews = WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,"[class='item']")))
-        result['product_name'] = driver.find_element(By.CSS_SELECTOR, "[class='pdp-mod-product-badge-title']").text
+        # driver.implicitly_wait(5)
+        # driver.execute_script("window.scrollTo(0, document.body.scrollHeight*0.8);")
+        try:
+            product_reviews = WebDriverWait(driver,2).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,"[class='item']")))
+        except:
+            break
         result['avg_rating'] = driver.find_element(By.CSS_SELECTOR, "[class='score-average']").text
 
         # Get product review
@@ -54,19 +57,17 @@ def scrape_lazada(driver, url, max_comment = 5):
 
         if x < max_comment:
             #Check for next button to click. If no button found, exit loop.
-            if len(driver.find_elements(By.CSS_SELECTOR,"button.next-pagination-item.next[disabled]")) > 0:
+            if len(driver.find_elements(By.CSS_SELECTOR,"button.next-pagination-item.next[disabled]")) == 0:
                 break
             else:
                 try:
-                    button_next = WebDriverWait(driver, 5).until(
+                    button_next = WebDriverWait(driver, 2).until(
                         EC.visibility_of_element_located((By.CSS_SELECTOR, "button.next-pagination-item.next")))
                     driver.execute_script("arguments[0].click();", button_next)
-                    driver.implicitly_wait(10)
+                    continue
                 except: break
         else:
             break
-
-
 
     return result
 
@@ -83,7 +84,6 @@ def scrape_lazada_by_product(driver, product_name, max_page = 5, max_comment_per
     for a in soup.find_all('a', href=True):
         if '/url?q=https://www.lazada.vn/products/' in a['href']:
             prod_links.append(a['href'][7:])
-    print(len(prod_links))
     limit = min(max_page, len(prod_links))
     for i in range(limit):
         result["result"].append(scrape_lazada(driver, prod_links[i], max_comment_per_page))
@@ -91,15 +91,15 @@ def scrape_lazada_by_product(driver, product_name, max_page = 5, max_comment_per
 
 if __name__ == '__main__':
     try:
-        driver = webdriver.Chrome()
+        driver = webdriver.Chrome("D:/chromedriver.exe")
     except:
         from webdriver_manager.chrome import ChromeDriverManager
         driver = webdriver.Chrome(ChromeDriverManager().install())
-    #driver = webdriver.Edge()
+
     # url = r'https://www.lazada.vn/products/dien-thoai-apple-iphone-13-pro-max-128gb-i1522497182-s6393590575.html?search=1&spm=a2o4n.searchlistcategory.list.i72.75bf3a1fbXB2jM'
     # test = scrape_lazada(driver, url, 4)
 
-    test = scrape_lazada_by_product(driver, "iphone 13")
+    test = scrape_lazada_by_product(driver, "tủ lạnh LG")
 
     driver.close()
     test_result = json.dumps(test, indent=4, ensure_ascii=False)
